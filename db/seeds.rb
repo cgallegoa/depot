5f9e7cd5694d1e59805f1b28b5022a85a3c96e5b
Carlos <cgallegoa@mastersoft3000.com>
2014-04-25 22:40:17 -0500

    sexta subida

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,3 +53,49 @@ Product.create!(title: 'Rails Test Prescriptions',
       </p>},
   image_url: 'rtp.jpg',
   price: 34.95)
+
+
+
+# . . .
+
+Product.create!(title: 'Zapatos para Gato Jetón',
+  description: 
+    %{<p>
+        <em>Bla ble ble blo blu, áéíóúñÑ , Rails Test Prescriptions</em> is a comprehensive guide to testing
+        Rails applications, covering Test-Driven Development from both a
+        theoretical perspective (why to test) and from a practical perspective
+        (how to test effectively). It covers the core Rails testing tools and
+        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+      </p>},
+  image_url: 'logo.png',
+  price: 40.00)
+# . . .
+
+Product.create!(title: 'Radio Reloj',
+  description: 
+    %{<p>
+        <em>Maravilloso radio reloj. Con la compra de uno le regalamos el radio y el reloj,
+         Rails Test Prescriptions</em> is a comprehensive guide to testing
+        Rails applications, covering Test-Driven Development from both a
+        theoretical perspective (why to test) and from a practical perspective
+        (how to test effectively). It covers the core Rails testing tools and
+        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+      </p>},
+  image_url: 'rails.png',
+  price: 13.25)
+# . . .
+
+Product.create!(title: 'Telelevisor 750 pulgadas.',
+  description: 
+    %{<p>
+        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
+        Rails applications, covering Test-Driven Development from both a
+        theoretical perspective (why to test) and from a practical perspective
+        (how to test effectively). It covers the core Rails testing tools and
+        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+      </p>},
+  image_url: 'rtp.jpg',
+  price: 8.50)
